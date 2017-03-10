409c882ff223faf82e0787a2f1c7e4766fea46ee
Jules LASNE <jlasne@student.42.fr>
2017-03-10 10:34:37 +0100

    minimised the what_cmd cmd and split it

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 10:45:49 by jlasne            #+#    #+#             */
-/*   Updated: 2017/03/10 10:29:06 by jlasne           ###   ########.fr       */
+/*   Updated: 2017/03/10 10:34:03 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,26 @@ void	env(char ***env, char **input)
 		ft_print_array(*env);
 }
 
-void	what_cmd(char **input, char ***envcpy, int size, char **tmp_path)
+void	what_cmd_helper(char **tmp_path, int size, char **input, char ***envcpy)
 {
 	int ok;
 
 	ok = 0;
+	if (ft_getenv("PATH", *envcpy) == -1)
+		ft_printf("Minishell: Command not found: %s\n", input[0]);
+	else
+	{
+		ok = test_access(tmp_path, size);
+		if (ok == -1)
+			ft_printf("Minishell: Command not found: %s\n", input[0]);
+		else
+			cmd_exec(tmp_path[ok], input, *envcpy);
+	}
+
+}
+
+void	what_cmd(char **input, char ***envcpy, int size, char **tmp_path)
+{
 	if (ft_strcmp(input[0], "cd") == 0)
 		cmd_cd(input, envcpy);
 	else if (ft_strcmp(input[0], "env") == 0)
@@ -52,18 +67,7 @@ void	what_cmd(char **input, char ***envcpy, int size, char **tmp_path)
 	else if (ft_strcmp(input[0], "help") == 0)
 		disp_help();
 	else
-	{
-		if (ft_getenv("PATH", *envcpy) == -1)
-			ft_printf("Minishell: Command not found: %s\n", input[0]);
-		else
-		{
-			ok = test_access(tmp_path, size);
-			if (ok == -1)
-				ft_printf("Minishell: Command not found: %s\n", input[0]);
-			else
-				cmd_exec(tmp_path[ok], input, *envcpy);
-		}
-	}
+		what_cmd_helper(tmp_path, size, input, envcpy);
 }
 
 int		ft_default_env(char ***env)
